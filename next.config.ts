30090af8b88f20075de4762aac5fb0f7c5d8dafd
Prathamesh Kulkarni <prathameshkulkarni710@gmail.com>
2025-07-07 18:38:23 +0000

    improve caching

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,44 @@ const withPWA = withPWAInit({
   disable: false,
   register: true,
   skipWaiting: true,
-  cacheStartUrl: true,
-  dynamicStartUrl: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts',
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-font-assets',
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-image-assets',
+        expiration: {
+          maxEntries: 64,
+          maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        },
+      },
+    },
+  ],
+  fallbacks: {
+    document: '/offline',
+  }
 });
 
 const nextConfig: NextConfig = {
