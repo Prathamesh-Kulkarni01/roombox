754e1e71d529f0fa3794a439758d1d3a75171af7
Prathamesh Kulkarni <prathameshkulkarni710@gmail.com>
2025-10-13 17:07:10 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -100,6 +100,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.qrserver.com',
       }
     ],
   },
