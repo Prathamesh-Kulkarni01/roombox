9bee48222849d07bc9225530bddbf0152031457b
Prathamesh-Kulkarni01 <prathameshkulkarni710@gmail.com>
2026-04-17 21:35:14 -0700

    del: Remove obsolete project files

--- check-env.ts
+++
@@ -1,4 +0,0 @@
-
-import { getEnv } from './src/lib/env';
-console.log('WHATSAPP_PHONE_NUMBER_ID:', getEnv('WHATSAPP_PHONE_NUMBER_ID', 'not set').substring(0, 5) + '...');
-console.log('WHATSAPP_ACCESS_TOKEN:', getEnv('WHATSAPP_ACCESS_TOKEN', 'not set').length > 10 ? 'present' : 'not set');
